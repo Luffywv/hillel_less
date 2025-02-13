e376166e613a953790fef598430a7bedaebdda9c
Alex <polyakkw@gmail.com>
2025-02-13 22:42:26 +0200

    init

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-user_number = int(input("Введите  целое число: "))
-print(f"Ваше целое число: {user_number} ")
+ # Ввод данных
+user_str1 = input("Введите первое слово: ")
+user_str2 = input("Введите второе слово: ")
+# Вывод в обратном порядке
+reverse_order = user_str1[::-1] +"" "" + user_str2
+# Вывод значений
+print(f"Вывод данных: {reverse_order}")
+
+
