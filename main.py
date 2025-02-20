595ba5050aec35a23ca5b85d6309e0557b45d0ba
Sasha <polyakkw@gmail.com>
2025-02-20 19:00:28 +0200

    Update main.py
    Если непарное число элементов

--- main.py
+++ main.py
@@ -9,5 +9,16 @@ else:
 
 print(result)
 
+# Если не парное число  элементов 
 
+original_list = [1, 2, 3, 4, 5]
+if len(original_list) == 0:
+    result = [[], []]
+elif len(original_list) == 1:
+    result = [original_list[:1], []]
+else:
+    middle = len(original_list) // 2  # 5 // 2 = 2
+    result = [original_list[:middle], original_list[middle:]]  # [:2] і [2:]
+
+print(result)  # [[1, 2], [3, 4, 5]]
 
