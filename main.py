5fc9f76257fac19b5626760f8d7e2d6c17e554c7
Sasha <polyakkw@gmail.com>
2025-02-23 22:12:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-arr = [0, 1, 7, 2, 4, 8]
-result = sum(arr[i] for i in range(0, len(arr), 2)) * arr[-1] if arr else 0
-print(f"Input: {arr}, Result: {result}")
+import random
+#  Создаём список 
+length = random.randint(3, 10)
+original_list = [random.randint(1, 100) for _ in range(length)] 
 
-arr = [1, 3, 5]
-result = sum(arr[i] for i in range(0, len(arr), 2)) * arr[-1] if arr else 0
-print(f"Input: {arr}, Result: {result}")  
-
-arr = [6]
-result = sum(arr[i] for i in range(0, len(arr), 2)) * arr[-1] if arr else 0
-print(f"Input: {arr}, Result: {result}")  
-
-arr = []
-result = sum(arr[i] for i in range(0, len(arr), 2)) * arr[-1] if arr else 0
-print(f"Input: {arr}, Result: {result}") 
+# Выбираем нужные элементы 
+new_list = [original_list[0], original_list[2], original_list[-2]]
+# Вывод результата 
+print(f"Оригінальний список (довжина {len(original_list)}): {original_list}")
+print(f"Новий список: {new_list}")
